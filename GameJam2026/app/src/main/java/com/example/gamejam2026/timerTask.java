2374b2c4fad99a0af36e546625e91bfd7970c66b
DanLouble <128084601+DanLouble@users.noreply.github.com>
2026-09-25 18:58:40 +0100

    Update timerTask.java

--- GameJam2026/app/src/main/java/com/example/gamejam2026/timerTask.java
+++ GameJam2026/app/src/main/java/com/example/gamejam2026/timerTask.java
@@ -9,30 +9,30 @@ public class timerTask {
 
 	public static void main(String[] args) {
 		//receives input from user
-    		Scanner timeObject = new Scanner(System.in);
-    		System.out.print("Enter duration (hh:mm:ss): ");
-    		String timeInput = timeObject.nextLine();
-    		
-			//setting parameters for schedule method
-			int delay = 1000;
-    		int period = 1000;
-			//setting timer object with Timer library constructor
-    		timer = new Timer();
+    	Scanner timeObject = new Scanner(System.in);
+		System.out.print("Enter duration (hh:mm:ss): ");
+		String timeInput = timeObject.nextLine();
 		
-			//parsing in the input to the timer method
-			int hours = Integer.parseInt(timeInput.substring(0,2));
-			int minutes = Integer.parseInt(timeInput.substring(3,5));
-			int seconds = Integer.parseInt(timeInput.substring(6,8));
- 
-    		interval = hours*60*60 + minutes*60 + seconds;
-    		//System.out.println(hours + ":" + minutes + ":" + seconds);
-    		timer.scheduleAtFixedRate(new TimerTask() {
+		//setting parameters for schedule method
+		int delay = 1000;
+		int period = 1000;
+		//setting timer object with Timer library constructor
+		timer = new Timer();
+	
+		//parsing in the input to the timer method
+		int hours = Integer.parseInt(timeInput.substring(0,2));
+		int minutes = Integer.parseInt(timeInput.substring(3,5));
+		int seconds = Integer.parseInt(timeInput.substring(6,8));
 
-        		public void run() {
-					int outputTime = setInterval();
-					System.out.println((((outputTime/(60*60)) < 10) ? "0" : "") + outputTime/(60*60) + (((outputTime/60)%60 < 10) ? ":0" : ":") + (outputTime/60)%60 + (((outputTime%60) < 10) ? ":0" : ":") + outputTime%60);
-        		}
-    		}, delay, period);
+		interval = hours*60*60 + minutes*60 + seconds;
+		//System.out.println(hours + ":" + minutes + ":" + seconds);
+		timer.scheduleAtFixedRate(new TimerTask() {
+
+			public void run() {
+				int outputTime = setInterval();
+				System.out.println((((outputTime/(60*60)) < 10) ? "0" : "") + outputTime/(60*60) + (((outputTime/60)%60 < 10) ? ":0" : ":") + (outputTime/60)%60 + (((outputTime%60) < 10) ? ":0" : ":") + outputTime%60);
+			}
+		}, delay, period);
 	}
 
 //end countdown when seconds left reaches 0
