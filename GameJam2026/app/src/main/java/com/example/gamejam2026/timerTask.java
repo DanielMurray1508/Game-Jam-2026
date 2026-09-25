dd7d709985d1a9e3b973c97f3d839c3f403a9d16
DanLouble <128084601+DanLouble@users.noreply.github.com>
2026-09-25 18:57:37 +0100

    Update and rename javatask.java to timerTask.java

--- GameJam2026/app/src/main/java/com/example/gamejam2026/timerTask.java
+++ GameJam2026/app/src/main/java/com/example/gamejam2026/timerTask.java
@@ -13,16 +13,16 @@ public class timerTask {
     		System.out.print("Enter duration (hh:mm:ss): ");
     		String timeInput = timeObject.nextLine();
     		
-		//setting parameters for schedule method
-		int delay = 1000;
+			//setting parameters for schedule method
+			int delay = 1000;
     		int period = 1000;
-		//setting timer object with Timer library constructor
+			//setting timer object with Timer library constructor
     		timer = new Timer();
 		
-		//parsing in the input to the timer method
-		int hours = Integer.parseInt(timeInput.substring(0,2));
-		int minutes = Integer.parseInt(timeInput.substring(3,5));
-		int seconds = Integer.parseInt(timeInput.substring(6,8));
+			//parsing in the input to the timer method
+			int hours = Integer.parseInt(timeInput.substring(0,2));
+			int minutes = Integer.parseInt(timeInput.substring(3,5));
+			int seconds = Integer.parseInt(timeInput.substring(6,8));
  
     		interval = hours*60*60 + minutes*60 + seconds;
     		//System.out.println(hours + ":" + minutes + ":" + seconds);
